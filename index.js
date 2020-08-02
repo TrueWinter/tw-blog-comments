338329f4b15fff4e405653eaf3eedb5922f78ca5
NdT3Development <nicholis@ndt3.me>
2020-08-03 00:58:24 +0200

    need a way to verify that i'm commenting as TW and not someone else

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ var isBase64 = require('is-base64');
 var base64 = require("js-base64");
 const GoogleRecaptcha = require('google-recaptcha');
 var cors = require('cors');
+var fs = require('fs');
+var cookieParser = require('cookie-parser');
 var express = require('express');
 var app = express();
 
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors());
+app.use(cookieParser());
+
+var loginCookies = fs.readFileSync('./login-cookies.json');
 
 const googleRecaptcha = new GoogleRecaptcha({secret: '***REMOVED***-Ak1L'});
 
@@ -46,6 +51,10 @@ app.post('/comment', function (req, res) {
 				comment: xss(req.body.comment),
 				time: moment().utc().format('D MMMM YYYY h:mm A Z')
 			};
+			
+			if (req.cookies['tw-comments-login'] && loginCookies.includes(req.cookies['tw-comments-login'])) {
+				document.isTrueWinter = true;
+			}
 
 			collection.insertOne(document).then(function() {
 				console.log('data inserted');
@@ -84,6 +93,10 @@ app.post('/reply', function (req, res) {
 			comment: xss(req.body['tw-rf-comment']),
 			time: moment().utc().format('D MMMM YYYY h:mm A Z')
 		};
+		
+		if (req.cookies['tw-comments-login'] && loginCookies.includes(req.cookies['tw-comments-login'])) {
+			document.isTrueWinter = true;
+		}
 
 		collection.insertOne(document).then(function() {
 			console.log('data inserted');
@@ -154,35 +167,36 @@ app.get('/comments/:post', function(req, res) {
 	
 });
 
-/*app.get('/replies/:post', function(req, res) {
-	if (!req.params.post) {
-		return res.json({success: false, message: 'Post parameter required'});
+var logins = {
+	'nicholis': '***REMOVED***'
+};
+
+app.get('/login', function(req, res) {
+	res.end('<form method="post"><input type="text" name="username" placeholder="Username"><br><input type="password" name="password" placeholder="Password"><br><button type="submit">Login</button></form>');
+});
+
+app.get('/login/verify/:hash', function(req, res) {
+	var l = JSON.parse(loginCookies);
+	res.end(l.includes(req.params.hash));
+});
+
+app.post('/login', function(req, res) {
+	if (req.cookies['tw-comments-login'] && loginCookies.includes(req.cookies['tw-comments-login'])) return res.end('Already logged in');
+	if (logins[req.body.username] && logins[req.body.username] === req.body.password) {
+		const cookieValue = md5(uuid.v4())+md5(uuid.v4())+md5(uuid.v4());
+		res.cookie('tw-comments-login', cookieValue, { expires: new Date(Date.now() + 5000000000) });
+		res.cookie('tw-comments-logged-in', 'true', { domain: '.truewinter.dev', expires: new Date(Date.now() + 5000000000) });
+		var lCTmp = JSON.parse(loginCookies);
+		console.log(lCTmp);
+		lCTmp.push(cookieValue);
+		console.log(lCTmp);
+		fs.writeFileSync('./login-cookies.json', JSON.stringify(lCTmp));
+		loginCookies = fs.readFileSync('./login-cookies.json');
+		res.end('OK');
+	} else {
+		res.status(401).end('Incorrect login');
 	}
-	if (!isBase64(req.params.post)) {
-		return res.json({success: false, message: 'Post parameter not base64 encoded'});
-	}
-	
-	var post = xss(base64.decode(decodeURIComponent(req.params.post)));
-	
-	const client = new MongoClient(uri, { useNewUrlParser: true });
-	
-	client.connect(err => {
-		const collection = client.db("tw-comments-01").collection("replies");
-		
-		collection.find({ post: post }).toArray(function(err, data) {
-			if (err) {
-				console.log(err);
-				return res.json({success: false, message: 'Error while fetching data'});
-			}
-			console.log(data);
-			res.json({success: true, data: data});
-			//res.json({success: true, message: 'Data inserted'});
-			//res.end();
-			client.close();
-		});
-	});
-	
-});*/
+});
 
 app.listen(8804, function() {
 	console.log('Listening on port 8804');
